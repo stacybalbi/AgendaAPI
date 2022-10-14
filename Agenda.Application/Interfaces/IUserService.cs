08f50ed943e0f6177ef6850dd4af3100bc60e294
Stacy Balbi <stacybalbip@gmail.com>
2022-10-14 14:53:22 -0400

    :)

--- Agenda.Application/Interfaces/IUserService.cs
+++ Agenda.Application/Interfaces/IUserService.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Agenda.Application.Generic.Interfaces;
 
 namespace Agenda.Application.Interfaces
 {
